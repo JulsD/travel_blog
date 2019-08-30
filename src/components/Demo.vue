39579fe20df548261f5528eb746b64b17847e40f
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2019-08-30 14:46:10 +0200

    add router samples

---
+++ src/components/Demo.vue
@@ -0,0 +1,30 @@
+// Demo
+<template>
+  <main>
+  <section>
+  <h1>Just starting the demo block</h1>
+  <p>Going to add some text here</p>
+  </section>
+  </main>
+</template>
+
+<script>
+export default {
+  computed: {
+    username () {
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+// Home
+<template>
+  <main>
+  <section>
+  <h1>Just starting the main block</h1>
+  <p>Going to add some text here</p>
+  </section>
+  </main>
+</template>
+
+<script>
+export default {
+  computed: {
+    username () {
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
