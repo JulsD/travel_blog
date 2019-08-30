aa4c4d53b5e633ef53b2ae762b380e191ba9e184
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2019-08-30 14:15:38 +0200

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: "development",
+  entry: ["./src/index.js"],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: "./dist"
+  },
   module: {
     rules: [
-      // ... другие правила
       {
         test: /\.vue$/,
         loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
   plugins: [
-    // убедитесь что подключили плагин!
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Travel blog',
+      hash: true,
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
   ]
 }
