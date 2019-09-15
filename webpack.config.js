1a71ace07a1da4bf940bf85accd78f1cc57c4724
JulsD <dutchak.julia@gmail.com>
2019-09-15 15:31:39 +0200

    basic project setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,69 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
+  entry: [
+    './src/app.js'
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  },
   module: {
     rules: [
-      // ... другие правила
       {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: file => (
+            /node_modules/.test(file) &&
+            !/\.vue\.js/.test(file)
+          )
+    },
+    {
         test: /\.vue$/,
         loader: 'vue-loader'
+    },
+    {
+        test: /\.css$/,
+        use: [
+            'vue-style-loader',
+            'css-loader'
+        ]
+    },
+    {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+    },
+    {
+        test: /\.styl(us)?$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
       }
     ]
   },
+  resolve: {
+    alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
   plugins: [
-    // убедитесь что подключили плагин!
-    new VueLoaderPlugin()
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Travel Blog',
+        template: path.resolve(__dirname, './src/index.html')
+      })
   ]
-}
+};
