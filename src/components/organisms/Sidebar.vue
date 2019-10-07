f0178ef819f1f05c7859d56ea758c0cf110dd6ae
JulsD <dutchak.julia@gmail.com>
2019-10-07 21:24:30 +0200

    add Sidebar component

---
+++ src/components/organisms/Sidebar.vue
@@ -0,0 +1,35 @@
+// Sidebar.component.vue
+<template>
+  <aside id="sidebar">
+      <div class="sidebar-container">
+        <form class="search-form"></form>
+        <div>
+            <h2>Tags</h2>
+            <span v-for="tag in tags" :key="tag">{{tag}}</span>
+        </div>
+        <div>Best for the last time</div>
+      </div>
+  </aside>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            tags: ['norway', 'netherlands', 'ukraine', 'amsterdam']
+        }
+    }
+}
+</script>
+
+<style>
+#sidebar {
+    padding: var(--gap-2);
+}
+
+.sidebar-container {
+    background-color: var(--primary-color-2-tint);
+    border-radius: var(--gap-2);
+    padding: var(--gap-3);
+}
+</style>
