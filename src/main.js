d48cf526efa6fa08569e33d1bffdba46726bba04
JulsD <dutchak.julia@gmail.com>
2019-10-07 23:36:25 +0200

    add authentication to the app

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import _ from 'lodash';
 import './styles';
 import './firebase';
 
+import firebase from './firebase';
+import auth from './services/auth.service';
 import router from './router';
 import store from './store';
 import App from './App.vue';
