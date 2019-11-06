0de58d6a4c30f0f6c43be9d41ff8f29039f61296
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2019-11-06 16:10:01 +0100

    add animation for logo image

--- src/components/molecules/Logo.vue
+++ src/components/molecules/Logo.vue
@@ -24,48 +24,33 @@ export default {
 <style>
 .logo {
     display: flex;
-    align-items: center;
+    align-items: flex-end;
     color: var(--text-color);
     font-size: 1.5rem;
     font-weight: bold;
     text-decoration: none;
 }
 
+.logo .ice-cream-wrapper {
+    font-size: 12px;
+    margin-right: var(--gap-1);
+}
+
 .logo:hover {
     text-decoration: none;
 }
 
-.logo [class^="icon-"] {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 45px;
-    width: 45px;
-    box-sizing: border-box;
-    font-size: inherit;
-    color: inherit;
-    padding: var(--gap-1);
-    position: relative;
+.logo:hover .ice-cream-ball {
+    --a-duration: 1s;
+    --a-delay: 0s;
+    animation: swing-vartical var(--a-duration) linear infinite var(--a-delay) alternate;
 }
-
-.logo [class^="icon-"]:after {
-    content: '';
-    display: block;
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    border-radius: 50%;
-    border: 2px dashed var(--text-color);
+.logo:hover .ice-cream-ball:nth-child(2) {
+    --a-duration: 2s;
+    --a-delay: .1s;
 }
-
-.logo:hover [class^="icon-"]:after {
-    animation: 
-    rotate-gorizontal 3s ease-out infinite .2s alternate-reverse;
-}
-
-.logo [class^="icon-"] + span {
-    margin-left: var(--gap-1);
+.logo:hover .ice-cream-ball:nth-child(2) {
+    --a-duration: .5s;
+    --a-delay: .2s;
 }
 </style>
