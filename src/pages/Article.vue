3b8b0478bbc833595b0d45842f035911d311913f
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2019-11-19 15:56:39 +0100

    create article form component

---
+++ src/pages/Article.vue
@@ -0,0 +1,39 @@
+// Article.component.vue
+<template>
+<PageWrapper>
+    <article class="article">
+        {{$route.params.name}}
+        <ArticleForm />
+        <!-- <header class="article-header">
+            <h1 class="article-title">{{article.title}}</h1>
+            <span class="article-date">{{article.created_at}}</span>
+        </header>
+        <p class="article-description">{{article.description}}</p>
+        <section v-for="(section, index) in article.body" :key="index">
+            <h2>section.title</h2>
+            <h2>section.body</h2>
+        </section> -->
+    </article>
+</PageWrapper>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+import PageWrapper from './PageWrapper.vue'
+import ArticleForm from '../components/molecules/ArticleForm.vue'
+
+export default {
+    mounted() {
+        // this.init()
+    },
+    computed: mapState('articles', ['articles']),
+    methods: mapActions('articles', ['init']),
+    components: {
+        PageWrapper,
+        ArticleForm
+  },
+}
+</script>
+
+<style>
+</style>
