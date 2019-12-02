835d26a12ff80eb91cf060724046a7c1c9bd0de1
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2019-12-02 17:28:58 +0100

    add article form

---
+++ src/pages/ArticleNew.vue
@@ -0,0 +1,28 @@
+// ArticleNew.component.vue
+<template>
+<PageWrapper>
+        {{$route.params.name}}
+        <ArticleForm />
+</PageWrapper>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+import PageWrapper from './PageWrapper.vue'
+import ArticleForm from '../components/molecules/ArticleForm.vue'
+
+export default {
+    mounted() {
+        // this.init()
+    },
+    computed: mapState('articles', ['articles']),
+    methods: mapActions('articles', ['init']),
+    components: {
+        PageWrapper,
+        ArticleForm
+  },
+}
+</script>
+
+<style>
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,17 @@ import { vuexfireMutations } from 'vuexfire'
 
 import auth from './auth'
 import articles from './articles'
+import article from './article'
+import tags from './tags'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     auth,
-    articles
+    articles,
+    article,
+    tags
   },
   mutations: {
     ...vuexfireMutations
