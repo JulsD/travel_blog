6a9838419ffbb969494616b7e7aa384ec0fc73bc
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2019-12-02 17:28:58 +0100

    add article form

--- src/router.js
+++ src/router.js
@@ -4,11 +4,13 @@ import VueRouter from 'vue-router';
 import Home from './pages/Home.vue'
 import Demo from './pages/Demo.vue'
 import Article from './pages/Article.vue'
+import ArticleNew from './pages/ArticleNew.vue'
 
 Vue.use(VueRouter);
 
 export default new VueRouter({routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/demo/:id', name: 'demo', component: Demo },
-    { path: '/articles/:name', name: 'article', component: Article }
+    { path: '/articles/:name', name: 'article', component: Article },
+    { path: '/new-article/:name', name: 'new-article', component: ArticleNew },
 ]})
